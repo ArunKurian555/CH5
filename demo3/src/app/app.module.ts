80d25526336df0f0f8c1db1cef3a59ff223d87d1
unknown <arunkurian4455@gmail.com>
2020-06-09 19:37:58 -0400

    view4 added

--- demo3/src/app/app.module.ts
+++ demo3/src/app/app.module.ts
@@ -10,10 +10,11 @@ import { ChRouteService } from 'src/service/ch-route.service';
 import { UtilitiesComponent } from './utilities/utilities.component';
 import { ConfigService } from 'src/service/config.service';
 import { View3Component } from './view3/view3.component';
-import { ScrollingModule } from '@angular/cdk/scrolling'
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { View4Component } from './view4/view4.component'
 
 @NgModule({
-  declarations: [AppComponent, ScenesComponent, UtilitiesComponent, View3Component],
+  declarations: [AppComponent, ScenesComponent, UtilitiesComponent, View3Component, View4Component],
   imports: [BrowserModule, FontAwesomeModule, ScrollingModule],
   providers: [
     { provide: APP_BASE_HREF, useValue: './' },
